8021e4fd3f423e9fbbf67071ed28fe2039877325
Mohsin Mamoon Hafiz <mohsin.mamoon@students.iiit.ac.in>
2019-10-18 23:49:09 +0530

    Seperated code for both nodes

--- Code/All_Sensors/All_Sensors_node_2/All_Sensors_node_2.ino
+++ Code/All_Sensors/All_Sensors_node_2/All_Sensors_node_2.ino
@@ -2,7 +2,7 @@
 //Libraries
 #include <ThingSpeak.h>
 #include "ESP8266WiFi.h"
-#include "DHT.h";
+#include "DHT.h"
 #include <Wire.h>
 #include "MutichannelGasSensor.h"
 #include <SDS011.h>
@@ -13,10 +13,6 @@
 
 
 //Constants
-#define DHT_22 0
-#define SDS 11
-#define GROVE 2
-#define GPS 3
 #define DHTPIN D4    // what pin we're connected to
 #define DHTTYPE DHT22   // DHT 22  (AM2302)
 #define node_RX D5
@@ -28,50 +24,52 @@
 TinyGPS gps;
 SoftwareSerial ss(gps_TX, gps_RX);
 SDS011 my_sds;
-DHT dht(DHTPIN, DHTTYPE); //// Initialize DHT sensor for normal 16mhz Arduino
-unsigned long Channel_ID = 864148;
-char API_KEY[] = "UKQQPFB6U0R8V4US";
+DHT dht(DHTPIN, DHTTYPE); // Initialize DHT sensor for normal 16mhz Arduino
+ unsigned long Channel_ID=886431;
+ char API_KEY[]="Y1GPJGG8VEFVTC0V";
 //char ssid[] = "JioFi_20E7D6C";
 //char psswd[] = "2wufpbqmx0";
-//char ssid[] = "MG5";
-//char psswd[] = "drowssap";
-char ssid[] = "Nitro 5";
-char psswd[] = "wvZu69eF";
+//char ssid[] = "esw-m19@iiith";
+//char psswd[] = "e5W-eMai@3!20hOct";
+//char ssid[] = "Nitro 5";
+//char psswd[] = "wvZu69eF";
+char ssid[] = "MGG5";
+char psswd[] = "drowssapp";
 WiFiClient  client;
 
 //Variables
 int chk, error;
 float hum, temp, p10, p25, c, flat, flon;
-String dht_22, sds, gas_val, gps_val;
+String data;
 unsigned long age;
 
 //ONEM2M
-int post(String data, int Sensor_type) {
-  String server = "http://10.1.129.146:8080";
+int post(String ae, String container, String data) {
+  String server = "http://139.59.42.21:8080";
   String cse = "/~/in-cse/in-name/";
-  String ae = "Outdoor_Air_pollution_mobile_6/";
-  String container;
-  if(Sensor_type == DHT_22) container = "DHT22";
-  else if(Sensor_type == SDS) container = "SDS011";
-  else if(Sensor_type == GROVE) container = "Grove_Gas";
-  else if(Sensor_type == GPS) container = "GPS";
-
   char m2m[200];
   String Data;
   Data = "{\"m2m:cin\": {"
     "\"con\":\"" + data + "\""
     "}}";
   HTTPClient http;
-  http.begin(server+cse+ae+container);
-  http.addHeader("X-M2M-Origin", "admin:admin");
-  http.addHeader("Content-type", "application/json;ty=4");
-  int response = http.POST(Data);
-  http.end();
-  return response;
+  Serial.println("\nConnecting to : " + server+cse+ae+container);
+  if(http.begin(server+cse+ae+container)) {
+    http.addHeader("X-M2M-Origin", "admin:admin");
+    http.addHeader("Content-type", "application/json;ty=4");
+    Serial.println("Uploading data: " + data);
+    int response = http.POST(Data);
+    http.end();
+    return response;
+  }
+  else {
+    return 1;
+  }
 }
-void update(int val, String sensor) {
-  if(val == 201) Serial.print(sensor + " values updated successfully\n");
-  else Serial.print("There was an error while uploading " + sensor +" values");
+
+void update(int val) {
+  if(val == 201) Serial.println("Data updated to OneM2M successfully\n");
+  else Serial.println("There was an error while uploading Data to OneM2M. Error Code: " + String(val));
 }
 
 static void smartdelay(unsigned long ms) {
@@ -84,40 +82,63 @@ static void smartdelay(unsigned long ms) {
 
 static void print_float(float val, float invalid, int len, int prec) {
   if (val == invalid) {
-    while (len-- > 1)
+    while (len-- > 1){
       Serial.print('*');
+      data = data + '*';
+    }
     Serial.print(' ');
   }
   else{
     Serial.print(val, prec);
+    data = data + String(val);
     int vi = abs((int)val);
     int flen = prec + (val < 0.0 ? 2 : 1); // . and -
     flen += vi >= 1000 ? 4 : vi >= 100 ? 3 : vi >= 10 ? 2 : 1;
     for (int i=flen; i<len; ++i)
       Serial.print(' ');
   }
+  data = data + ',';
   smartdelay(0);
 }
 
-void setup()
-{
+static void print_date(TinyGPS &gps) {
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  unsigned long age;
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+  if (age == TinyGPS::GPS_INVALID_AGE) {
+    Serial.print("********** ******** ");
+    data = data + "********** ******** ";
+  }
+  else {
+    char sz[32];
+    sprintf(sz, "%02d/%02d/%02d %02d:%02d:%02d ",
+        month, day, year, hour, minute, second);
+    Serial.print(sz);
+    data = data + sz;
+  }
+  smartdelay(0);
+}
+
+void setup(){
   pinMode(LED, OUTPUT);
   Serial.begin(115200);
   dht.begin();
   my_sds.begin(node_RX, node_TX);
   gas.begin(ADDR_I2C);
-  gas.powerOn(); 
-  Serial.println("Let's begin!");
+  gas.powerOn();  
+  Serial.println("Begin.");
+  Serial.println(WiFi.macAddress());
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   ThingSpeak.begin(client);
   digitalWrite(LED, HIGH);
   ss.begin(9600);
+  delay(10000);
 }
 
 
-void loop()
-{
+void loop() {
      if(WiFi.status() != WL_CONNECTED){
       digitalWrite(LED, HIGH);
       Serial.print("Attempting to connect to SSID: ");
@@ -131,40 +152,7 @@ void loop()
       Serial.println("\nConnected.");
       digitalWrite(LED, LOW);
     }
-    
-    c = gas.measure_CO();
-    Serial.print("The concentration of CO is ");
-    if(c>=0) Serial.print(c);
-    else Serial.print("invalid");
-    Serial.println(" ppm");
-    ThingSpeak.setField(3, c);
-    gas_val = String(c) + ",";
-    
-    c = gas.measure_NO2();
-    Serial.print("The concentration of NO2 is ");
-    if(c>=0) Serial.print(c);
-    else Serial.print("invalid");
-    Serial.println(" ppm");
-    ThingSpeak.setField(4, c);
-    gas_val = gas_val + String(c) + ",";
 
-    c = gas.measure_NH3();
-    Serial.print("The concentration of NH3 is ");
-    if(c>=0) Serial.print(c);
-    else Serial.print("invalid");
-    Serial.println(" ppm");
-    ThingSpeak.setField(5, c);
-    gas_val = gas_val + String(c);
-
-
-    error = my_sds.read(&p25, &p10);
-    if (!error) {
-      Serial.println("P2.5: " + String(p25) + "\t" + "P10:  " + String(p10));
-      ThingSpeak.setField(6, p25);
-      ThingSpeak.setField(7, p10);
-      sds = String(p25) + "," + String(p10);
-      
-    }
     
     hum = dht.readHumidity();
     temp= dht.readTemperature();
@@ -177,27 +165,64 @@ void loop()
     Serial.println(" Celsius");
     ThingSpeak.setField(1, hum);
     ThingSpeak.setField(2, temp);
-    dht_22 = String(hum) + "," + String(temp);
+    data = String(temp) + "," + String(hum) + ',';
+
+    
+    error = my_sds.read(&p25, &p10);
+    if (!error) {
+      Serial.println("P2.5: " + String(p25) + "\t" + "P10:  " + String(p10));
+      ThingSpeak.setField(6, p25);
+      ThingSpeak.setField(7, p10);
+      data = data + String(p25) + "," + String(p10) + ',';
+      
+    }
+    else {
+      data = data + "-1,-1,";
+    }
+//
+
+    c = gas.measure_CO();
+    Serial.print("The concentration of CO is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+    ThingSpeak.setField(3, c);
+    data = data + String(c) + ",";
+    
+    c = gas.measure_NO2();
+    Serial.print("The concentration of NO2 is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+    ThingSpeak.setField(4, c);
+    data = data + String(c) + ",";
+
+    c = gas.measure_NH3();
+    Serial.print("The concentration of NH3 is ");
+    if(c>=0) Serial.print(c);
+    else Serial.print("invalid");
+    Serial.println(" ppm");
+    ThingSpeak.setField(5, c);
+    data = data + String(c) + ',';
+
 
     gps.f_get_position(&flat, &flon, &age);
     print_float(flat, TinyGPS::GPS_INVALID_F_ANGLE, 10, 6);
     print_float(flon, TinyGPS::GPS_INVALID_F_ANGLE, 11, 6);
     Serial.println();
-    gps_val = String(flat) + ',' + String(flon);
-
+    print_date(gps);
     smartdelay(1000);
+    data = data + ",OAP4_2";
     
-//    update(post(dht_22, DHT_22), "DHT22");
-//    update(post(sds, SDS), "SDS011");
-//    update(post(gas_val, GROVE), "Grove Gas sensor");
-//    update(post(gps_val, GPS), "GPS");
-//    int response;
-//    if((response = ThingSpeak.writeFields(Channel_ID, API_KEY)) == 200) {
-//      Serial.println("Data updated successfully!");
-//    }
-//    else {
-//      Serial.println("Problem Updating channel!\nError code: " + String(response)); 
-//    }
-    delay(15000); //Delay 15 sec.
+    update(post("Team6_Outdoor_air_pollution_mobile/", "node_2", data));
+
+    int response;
+    if((response = ThingSpeak.writeFields(Channel_ID, API_KEY)) == 200) {
+      Serial.println("Data Updated to ThingSpeak successfully!");
+    }
+    else {
+      Serial.println("Problem Updating data for ThingSpeak!\nError code: " + String(response)); 
+    }
+    delay(10 * 60000); //Delay 60 * 10 sec.
     Serial.println("...\n\n\n\n\n\n\n");
 }
--- Code/make_tree.py
+++ Code/make_tree.py
@@ -3,7 +3,7 @@ from OneM2M import *
 server = 'http://127.0.0.1:8080'
 cse = '/~/in-cse/in-name/'
 ae = 'Outdoor_Air_pollution_mobile_6'
-containers = ["DHT22", "SDS011", "Grove_Gas", "GPS"]
+containers = ["Node1", "Node2"]
 
 print(register_ae(server+cse, ae, ['team_6', 'Sachin Chaudary', 'Rajashekar Reddy']))
 
